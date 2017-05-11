41478cce8b77a895ff7f6c2babd3e734ff451780
Li Wei <xmuweili@As-MacBook-Pro.hsd1.ca.comcast.net>
2017-05-11 00:08:09 -0700

    Update the configuration in CMakeLists for gradle build.

--- Install_OpenCV.sh
+++ Install_OpenCV.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# Download OpenCV and Install
+# Download OpenCV
 cd Libs && \
-curl https://svwh.dl.sourceforge.net/project/opencvlibrary/opencv-android/3.2.0/opencv-3.2.0-android-sdk.zip | tar -xf- -C .
+curl https://svwh.dl.sourceforge.net/project/opencvlibrary/opencv-android/3.2.0/opencv-3.2.0-android-sdk.zip | tar -xf- -C . && \
 
+# Copy OpenCV Jni Library to the android project
+cd .. && \
+mkdir app/src/main/jniLibs && \
+cp -R Libs/OpenCV-android-sdk/sdk/native/libs/. app/src/main/jniLibs/.
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ## Step 1: Install Android Studio
 ## Step 2: Install OpenCV
 ## Step 3: Build and Run!
+ninja: error: '../app/src/main/jniLibs/arm64-v8a/libopencv_java3.so', needed by '../../../../build/intermediates/cmake/debug/obj/arm64-v8a/libnative-lib.so', missing and no known rule to make it
