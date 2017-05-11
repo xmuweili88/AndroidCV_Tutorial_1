048346f8ad14ca31dd86af9ebe2af22411ed28b2
Li Wei <xmuweili@As-MacBook-Pro.hsd1.ca.comcast.net>
2017-05-10 23:09:07 -0700

    Add Install_OpenCV.sh for OpenCV support.

---
+++ Install_OpenCV.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Download OpenCV and Install
+cd Libs && \
+curl https://svwh.dl.sourceforge.net/project/opencvlibrary/opencv-android/3.2.0/opencv-3.2.0-android-sdk.zip | tar -xf- -C .
+
