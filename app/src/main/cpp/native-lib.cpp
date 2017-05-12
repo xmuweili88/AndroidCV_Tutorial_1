8a6d34d6fe29092644ddef4fbf58e32de4a6bdff
Li Wei <xmuweili@As-MacBook-Pro.hsd1.ca.comcast.net>
2017-05-11 21:38:56 -0700

    Draw a chess board in native C++ code.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,48 @@
 #include <jni.h>
 #include <string>
+#include <android/bitmap.h>
+#include <android/log.h>
+#include "opencv2/opencv.hpp"
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "native-lib", __VA_ARGS__)
+
+namespace {
+    // Draw the chess board on inupt cv::Mat image
+    void DrawCheessBoard(cv::Mat* image) {
+        const int width = image->cols;
+        const int height = image->rows;
+        const int block_size = 64;
+        uint8_t color = 0;
+        for(int i=0; i<width; i+=block_size){
+            uint8_t row_color = color;
+            color = ~color;
+            for (int j=0; j<height; j+=block_size) {
+                cv::rectangle(*image, cv::Rect(i, j, block_size, block_size),
+                              cv::Scalar(row_color, row_color, row_color, row_color), CV_FILLED);
+                row_color = ~row_color;
+            }
+        }
+    }
+}
 
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_androidcv_tutorial_11_MainActivity_stringFromJNI(
+JNIEXPORT void JNICALL
+Java_com_androidcv_tutorial_11_MainActivity_DrawChessBoardJNI(
         JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject /* this */, jobject bitmap) {
+    uint8_t *bitmapPixels;
+    AndroidBitmapInfo overlayInfo;
+    if(AndroidBitmap_getInfo(env, bitmap, &overlayInfo) < 0) {
+        LOGD("[ndk demo]AndroidBitmap_getInfo() failed");
+        return;
+    }
+    if (AndroidBitmap_lockPixels(env, bitmap, (void**)&bitmapPixels) < 0) {
+        LOGD("[ndk demo]AndroidBitmap_lockPixels() failed");
+        return;
+    }
+    cv::Mat bitmap_mat(overlayInfo.height, overlayInfo.width, CV_8UC4, bitmapPixels,
+                       overlayInfo.stride);
+    bitmap_mat.setTo(255);
+    DrawCheessBoard(&bitmap_mat);
+    AndroidBitmap_unlockPixels(env, bitmap);
 }
--- app/src/main/java/com/androidcv/tutorial_1/MainActivity.java
+++ app/src/main/java/com/androidcv/tutorial_1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.androidcv.tutorial_1;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,19 +15,46 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private TextureView texture_view;
+    private Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        texture_view = (TextureView) findViewById(R.id.texture_view);
+        texture_view.setSurfaceTextureListener(surfaceTextureListener);
     }
 
+    private TextureView.SurfaceTextureListener surfaceTextureListener =
+            new TextureView.SurfaceTextureListener(){
+                @Override
+                public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                    Canvas canvas = texture_view.lockCanvas();
+                    bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                    DrawChessBoardJNI(bitmap);
+                    canvas.drawBitmap(bitmap, 0, 0, null);
+                    texture_view.unlockCanvasAndPost(canvas);
+                }
+
+                @Override
+                public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+                }
+
+                @Override
+                public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                    return false;
+                }
+
+                @Override
+                public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+                }
+            };
     /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
+     * A native method that draw the chess board on input bitmap.
+     * This method is define in native-lib
      */
-    public native String stringFromJNI();
+    public native void DrawChessBoardJNI(Bitmap bitmap);
 }
