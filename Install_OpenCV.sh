506bf4d9c382e9502752815f0e50cfb132aee03d
L. W <developer.liwei@gmail.com>
2017-05-24 22:19:45 -0700

    Make dir for opencv library.

--- Install_OpenCV.sh
+++ Install_OpenCV.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Download OpenCV
+mkdir Libs && \
 cd Libs && \
 curl https://svwh.dl.sourceforge.net/project/opencvlibrary/opencv-android/3.2.0/opencv-3.2.0-android-sdk.zip | tar -xf- -C . && \
 
